3e67283c3157415721a744d98d03e26eece65e8c
RoopeVaarama <roope.vaarama@gmail.com>
2019-04-26 15:42:44 +0300

    ASD

--- app/src/main/java/com/example/gymapp/MainActivity.java
+++ app/src/main/java/com/example/gymapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.gymapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = new Intent(this, gymprogram.class);
+    }
+
+    public void onClick(View v){
+        startActivity(intent);
     }
 }
