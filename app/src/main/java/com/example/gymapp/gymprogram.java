478ba951efc384fa1271404ca5255dfd08f19058
RoopeVaarama <roope.vaarama@gmail.com>
2019-04-29 11:08:56 +0300

    push

--- app/src/main/java/com/example/gymapp/gymprogram.java
+++ app/src/main/java/com/example/gymapp/gymprogram.java
@@ -1,13 +1,21 @@
 package com.example.gymapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class gymprogram extends AppCompatActivity {
 
+    Intent pull;
+    Intent push;
+    Intent leg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gymprogram);
+        pull = new Intent(this, pullprogram.class);
+        push = new Intent(this, pushprogram.class);
+        leg = new Intent(this, legprogram.class);
     }
 }
